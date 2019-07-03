8fb36d11326758d32049e13dc22d73635cc750b8
Jaga1998 <44741841+Jaga1998@users.noreply.github.com>
2019-07-03 10:16:05 +0530

    Update set14.py

--- set14.py
+++ set14.py
@@ -1,7 +1,7 @@
 ch = input()
 if ch>='a' and ch<='z' or ch>='A' and ch<='Z':
  if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u':
-  print("Voewl")
+  print("Vowel")
  else:
   print("Consonant")
 else:
