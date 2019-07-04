0264d942f5d22f039d367f617df5fcb236a587ff
Jaga1998 <44741841+Jaga1998@users.noreply.github.com>
2019-07-04 14:56:41 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class reverse
+public class reverse
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{   Scanner sc= new Scanner(System.in);
