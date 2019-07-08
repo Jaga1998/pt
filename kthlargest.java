dd4de31c7101fdbc37dfecf7d91c8e12afb0fc00
Jaga1998 <44741841+Jaga1998@users.noreply.github.com>
2019-07-08 10:48:56 +0530

    Update kthlargest.java

--- kthlargest.java
+++ kthlargest.java
@@ -14,12 +14,12 @@ public class kthlargest
 		int k=sc.nextInt();
 		int arr[]=new int[n];
 		
-		for(int i=0;i<n;i++)
+		for(int i=1;i<n;i++)
 		{ 
 			arr[i]=sc.nextInt();
 		}
 	
 	Arrays.sort(arr);
-	System.out.println(arr[k-1]);
+	System.out.println(arr[k]);
 }
 }
