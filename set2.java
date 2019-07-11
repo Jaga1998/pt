f0674b488ed05f783c648ad4c98d33a720ddc90a
Jaga1998 <44741841+Jaga1998@users.noreply.github.com>
2019-07-11 10:50:40 +0530

    Create set2.java

---
+++ set2.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+public class Set2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		for(int j=0;j<k;j++)
+		{
+			int u=sc.nextInt();
+			int v=sc.nextInt();
+			int sum=0;
+			for(int i=u;i<v+1;i++)
+			{
+				sum=sum+arr[i];
+			}
+			System.out.println(sum);
+		}
+	}
+}
