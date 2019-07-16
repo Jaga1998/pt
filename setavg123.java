b11902537df0769723702a384a459d72071a21f9
Jaga1998 <44741841+Jaga1998@users.noreply.github.com>
2019-07-16 23:37:10 +0530

    Create setavg123.java

---
+++ setavg123.java
@@ -0,0 +1,43 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		int sum=0;int sum1=0;
+		int avg1=0;
+		int avg=0;
+		int i;
+		if(n%2==1)
+		{
+		for(i=0;i<n/2;i++)
+		{
+			sum=sum+arr[i];
+			
+		}
+		avg=(avg+sum)/n/2
+		for(i=(n+1)/2;i<n;i++)
+		{
+			sum1=sum1+arr[i];
+		
+		}
+		avg1=(avg+sum1)/2
+		if(avg1==avg&&avg1==arr[(n-1)/2
+		}
+		else
+		for(i=0;i<n/2;i++)
+		{
+			sum=sum+arr[i];
+			
+		}
+		avg=(avg+sum)/n/2
+	}
+}
