f92a127bcaf5b5afb1c2de7f837b5e59cc0d4c1f
Jaga1998 <44741841+Jaga1998@users.noreply.github.com>
2019-07-18 21:36:20 +0530

    Create set123.java

---
+++ set123.java
@@ -0,0 +1,36 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		
+		}
+		 int sum=0;
+		 int sum1=0;
+		 Arrays.sort(arr);
+		 for(int i=0;i<n;i=i+2)
+		 {
+		 	sum=sum+arr[i];
+		 }
+		 for(int i=1;i<n;i=i+2)
+	{
+		sum1=sum1+arr[i];
+	}
+		if(sum>sum1)
+		System.out.println(sum);
+		else
+		System.out.println(sum1);
+	}
+}
